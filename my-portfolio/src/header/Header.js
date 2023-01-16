8158c7bffc6274bc0c0a5eccf5ae1286ed6d2c1a
yasu hiromu <g020c1132@g.neec.ac.jp>
2023-01-16 23:20:17 +0900

    header change

--- my-portfolio/src/header/Header.js
+++ my-portfolio/src/header/Header.js
@@ -1,41 +1,109 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Avatar from '@mui/material/Avatar';
-import Image01 from './ap.jpg';
+import Button from '@mui/material/Button';
 
-export default function MenuAppBar() {
-    const [auth] = React.useState(true);
+const drawerWidth = 240;
+const navItems = [{name:'Home', link:'https://mui.com/material-ui/react-button/#main-content'}, /*nameにヘッダー右側の各名前を入れる、listにlinkを格納しておく*/
+                    {name:'About'}, 
+                    {name:'Contact'}];
+
+function DrawerAppBar(props) {
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+        <Typography variant="h6" sx={{ my: 2 }}>
+        </Typography>
+        <Divider />
+        <List>
+            {navItems.map((item) => (
+            <ListItem key={item} disablePadding>
+                <ListItemButton sx={{ textAlign: 'center' }}>
+                <ListItemText primary={item.name} />
+                </ListItemButton>
+            </ListItem>
+            ))}
+        </List>
+        </Box>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+        <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar component="nav">
             <Toolbar>
             <IconButton
-                size="large"
-                edge="start"
                 color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: 'none' } }}
             >
                 <MenuIcon />
             </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
                 Hiromu Yasukaga | 日本工学院八王子専門学校
             </Typography>
-            {auth && (
-                <div>
-                <IconButton>
-                    <Avatar src={Image01}/> {/*直接画像のパスを入れるとだめ。一回importしてから*/}
-                </IconButton>
-                </div>
-            )}
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                {navItems.map((item) => (
+                <Button href={item.link} key={item} sx={{ color: '#fff' }}> {/*hrefで画面遷移させる あらかじめlistにlinkを格納しておく*/}
+                    {item.name}
+                </Button>
+                ))}
+            </Box>
             </Toolbar>
         </AppBar>
+        <Box component="nav">
+            <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+                display: { xs: 'block', sm: 'none' },
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+            >
+            {drawer}
+            </Drawer>
+        </Box>
         </Box>
     );
 }
+
+DrawerAppBar.propTypes = {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+};
+
+export default DrawerAppBar;
