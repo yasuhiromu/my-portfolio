b3cca8cd5ffc26b6afe7e39d84d655c40660fcaa
Hiromu <g020c1132@g.neec.ac.jp>
2023-02-18 15:09:18 +0900

    eight commit

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -4,8 +4,9 @@ import React from 'react';
 // import Image00 from './image/head01.jpg' 
 import Button from '@mui/material/Button';
 import Header from './header/Header'
-import Header2 from './header/Header2'
+import Header2 from './header/Header-slider'
 import Port from './portfolio/Portfolio'
+import Portunder1000 from './portfolio/Portfolio-under1000'
 import Image01 from './header/ap.jpg'
 
 function App() {
@@ -14,119 +15,218 @@ function App() {
       <header className="App-header">
         <Header/>
       </header>
-      <div className="slider">
-        <Header2/>
-      </div>
-
-
-      <MediaQuery query="(max-width: 999px)">
-        <div>Screen is wide2: 😢</div>
-      </MediaQuery>
-      <MediaQuery query="(min-width: 1000px)">
-        <div>Screen is wide2: 😃</div>
-      </MediaQuery>
-
       
-      {/* <img src={Image00} alt="Logo" width="100%"/> オーストラリアで撮った画像にする */}
-      <div className='profile'>
-        <h1 id='name'>Hiromu Yasukaga | AIシステム科-ITスペシャリスト科</h1><br/>
-        <div className='my-profile'>
+      <MediaQuery query="(max-width: 999px)">
+          <div className="slider">
+            <Header2/>
+          </div>
+          {/* <img src={Image00} alt="Logo" width="100%"/> オーストラリアで撮った画像にする */}
+          <h1 id='name'>Hiromu Yasukaga <br/> AIシステム科-ITスペシャリスト科</h1><br/>
+            
+            <div className='my-profile'>
+              <h2>2001年生まれ21歳</h2>
+            
+              <p className='profile-text'>
+                パソコンを触り始めて3年が経ちました。<br/>
+                私は高校生2年生の春にオーストラリアへホームステイへ行かせていただきました。<br/>
+                その際に学生さんがプログラミングでゲームを作成していた様子をみて
+                プログラミングに興味を持ち始めました。<br/>
+                好奇心旺盛な性格で、気になったことを全力でやるのが好きなので、
+                海外に行ってみたり、コンテストやイベントへの参加、教室を開くなど他にも色々なことに挑戦してきました。
+              </p>
+            </div>
+            <div  className="myphoto">
+              <img src={Image01} alt="Logo" width="30%"/> {/*オーストラリアで撮った画像にする */}
+            </div>
+
+          <h2 className='port-title'>◆Portfolio</h2><br/>
+          <div className='my-port'>
+            
+            {/* ↓自分の作品をまとめる */}
+            <Portunder1000/>
+            
+          </div>  
+            
+            <h3 className='interne-title'><b>長期インターン</b></h3>
+            <div className='interne'>
+              <div className='frontend'>
+                <ul className='cc'>
+                  <p>フロントエンド</p>
+                    <ul>
+                      <li>Auth0</li>
+                      <li>pay.jp</li>
+                      <li>smartLP</li>
+                    </ul>
+                </ul>
+              </div>
+              <div className='backend'>
+                <ul className='cc'>
+                  <p>バックエンド</p>
+                    <ul>
+                      <li>wasabi</li>
+                      <li>kintone</li>
+                      <li>Zendesk</li>
+                    </ul>
+                </ul>
+              </div>
+              <div className='tool'>
+                <ul className='cc'>
+                  <p>情報共有ツール</p>
+                    <ul>
+                      <li>discode</li>
+                      <li>slack</li><br/>
+                    </ul>
+                </ul>
+              </div>
+              <div className='task'>
+                <ul className='cc'>
+                  <p>タスク管理ツール</p>
+                    <ul>
+                      <li>asana</li>
+                      <li>Instagant</li>
+                      <li>Github</li>
+                    </ul>
+                </ul>
+              </div>
+              <div className='lang'>
+                <ul className='cc'>
+                  <p>プログラミング言語</p>
+                    <ul>
+                      <li>React</li><br/><br/>
+                    </ul>
+                </ul>
+              </div>
+            </div>
           
-          <h2>
-            2001年生まれ21歳
-          </h2>
-          <p className='profile-text'>
-            パソコンを触り始めて3年が経ちました。<br/>
-            私は高校生2年生の春にオーストラリアへホームステイへ行かせていただきました。<br/>
-            その際に学生さんがプログラミングでゲームを作成していた様子をみて
-            プログラミングに興味を持ち始めました。<br/>
-            好奇心旺盛な性格で、気になったことを全力でやるのが好きなので、
-            海外に行ってみたり、コンテストやイベントへの参加、教室を開くなど他にも色々なことに挑戦してきました。
+
+          <hr className="hr-text"></hr>
+          <hr className="hr-text"></hr>
+
+          <div className='my-hobby'>
+            <h2 className='hobby-title'>◆hobby</h2><br/>
+            {/* ↓自分の趣味をまとめる */}
+            <p className='hobby-text'>
+                パソコンを触り始めて3年が経ちました。<br/>
+                私は高校生2年生の春にオーストラリアへホームステイへ行かせていただきました。<br/>
+                その際に学生さんがプログラミングでゲームを作成していた様子をみて
+                プログラミングに興味を持ち始めました。<br/>
+                好奇心旺盛な性格で、気になったことを全力でやるのが好きなので、
+                海外に行ってみたり、コンテストやイベントへの参加、教室を開くなど他にも色々なことに挑戦してきました。
+            </p>
+          </div>
+          <div id='set'>
+            <Button variant="contained" onClick={()=>{abc();}}>Hello World</Button>
+          </div>
+      </MediaQuery>
+
+
+
+
+      <MediaQuery query="(min-width: 1000px)">
+        <div className="slider">
+          <Header2/>
+        </div>
+        {/* <img src={Image00} alt="Logo" width="100%"/> オーストラリアで撮った画像にする */}
+          <h1 id='name'>Hiromu Yasukaga | AIシステム科-ITスペシャリスト科</h1><br/>
+          <div className='my-profile'>
+            
+            <h2>
+              2001年生まれ21歳
+            </h2>
+            
+            <p className='profile-text'>
+              パソコンを触り始めて3年が経ちました。<br/>
+              私は高校生2年生の春にオーストラリアへホームステイへ行かせていただきました。<br/>
+              その際に学生さんがプログラミングでゲームを作成していた様子をみて
+              プログラミングに興味を持ち始めました。<br/>
+              好奇心旺盛な性格で、気になったことを全力でやるのが好きなので、
+              海外に行ってみたり、コンテストやイベントへの参加、教室を開くなど他にも色々なことに挑戦してきました。
+            </p>
+          </div>
+          <div  className="myphoto">
+            <img src={Image01} alt="Logo" width="30%"/> {/*オーストラリアで撮った画像にする */}
+          </div>
+
+        <h2 className='port-title'>◆Portfolio</h2><br/>
+        <div className='my-port'>
+          
+          {/* ↓自分の作品をまとめる */}
+          <Port/>
+          
+        </div>  
+          
+          <h3 className='interne-title'><b>長期インターン</b></h3>
+          <div className='interne'>
+            <div className='frontend'>
+              <ul className='cc'>
+                <p>フロントエンド</p>
+                  <ul>
+                    <li>Auth0</li>
+                    <li>pay.jp</li>
+                    <li>smartLP</li>
+                  </ul>
+              </ul>
+            </div>
+            <div className='backend'>
+              <ul className='cc'>
+                <p>バックエンド</p>
+                  <ul>
+                    <li>wasabi</li>
+                    <li>kintone</li>
+                    <li>Zendesk</li>
+                  </ul>
+              </ul>
+            </div>
+            <div className='tool'>
+              <ul className='cc'>
+                <p>情報共有ツール</p>
+                  <ul>
+                    <li>discode</li>
+                    <li>slack</li><br/>
+                  </ul>
+              </ul>
+            </div>
+            <div className='task'>
+              <ul className='cc'>
+                <p>タスク管理ツール</p>
+                  <ul>
+                    <li>asana</li>
+                    <li>Instagant</li>
+                    <li>Github</li>
+                  </ul>
+              </ul>
+            </div>
+            <div className='lang'>
+              <ul className='cc'>
+                <p>プログラミング言語</p>
+                  <ul>
+                    <li>React</li><br/><br/>
+                  </ul>
+              </ul>
+            </div>
+          </div>
+        
+
+        <hr className="hr-text"></hr>
+        <hr className="hr-text"></hr>
+
+        <div className='my-hobby'>
+          <h2 className='hobby-title'>◆hobby</h2><br/>
+          {/* ↓自分の趣味をまとめる */}
+          <p className='hobby-text'>
+              パソコンを触り始めて3年が経ちました。<br/>
+              私は高校生2年生の春にオーストラリアへホームステイへ行かせていただきました。<br/>
+              その際に学生さんがプログラミングでゲームを作成していた様子をみて
+              プログラミングに興味を持ち始めました。<br/>
+              好奇心旺盛な性格で、気になったことを全力でやるのが好きなので、
+              海外に行ってみたり、コンテストやイベントへの参加、教室を開くなど他にも色々なことに挑戦してきました。
           </p>
         </div>
-        <div  className="myphoto">
-          <img src={Image01} alt="Logo" width="30%"/> {/*オーストラリアで撮った画像にする */}
+        <div id='set'>
+          <Button variant="contained" onClick={()=>{abc();}}>Hello World</Button>
         </div>
-      </div>
-
-      <h2 className='port-title'>◆Portfolio</h2><br/>
-      <div className='my-port'>
-        
-        {/* ↓自分の作品をまとめる */}
-        <Port/>
-        
-      </div>  
-        
-        <h3 className='interne-title'><b>長期インターン</b></h3>
-        <div className='interne'>
-          <div className='frontend'>
-            <ul className='cc'>
-              <p>フロントエンド</p>
-                <ul>
-                  <li>Auth0</li>
-                  <li>pay.jp</li>
-                  <li>smartLP</li>
-                </ul>
-            </ul>
-          </div>
-          <div className='backend'>
-            <ul className='cc'>
-              <p>バックエンド</p>
-                <ul>
-                  <li>wasabi</li>
-                  <li>kintone</li>
-                  <li>Zendesk</li>
-                </ul>
-            </ul>
-          </div>
-          <div className='tool'>
-            <ul className='cc'>
-              <p>情報共有ツール</p>
-                <ul>
-                  <li>discode</li>
-                  <li>slack</li><br/>
-                </ul>
-            </ul>
-          </div>
-          <div className='task'>
-            <ul className='cc'>
-              <p>タスク管理ツール</p>
-                <ul>
-                  <li>asana</li>
-                  <li>Instagant</li>
-                  <li>Github</li>
-                </ul>
-            </ul>
-          </div>
-          <div className='lang'>
-            <ul className='cc'>
-              <p>プログラミング言語</p>
-                <ul>
-                  <li>React</li><br/><br/>
-                </ul>
-            </ul>
-          </div>
-        </div>
+      </MediaQuery>
       
-
-      <hr className="hr-text"></hr>
-      <hr className="hr-text"></hr>
-
-      <div className='my-hobby'>
-        <h2 className='hobby-title'>◆hobby</h2><br/>
-        {/* ↓自分の趣味をまとめる */}
-        <p className='hobby-text'>
-            パソコンを触り始めて3年が経ちました。<br/>
-            私は高校生2年生の春にオーストラリアへホームステイへ行かせていただきました。<br/>
-            その際に学生さんがプログラミングでゲームを作成していた様子をみて
-            プログラミングに興味を持ち始めました。<br/>
-            好奇心旺盛な性格で、気になったことを全力でやるのが好きなので、
-            海外に行ってみたり、コンテストやイベントへの参加、教室を開くなど他にも色々なことに挑戦してきました。
-        </p>
-      </div>
-      <div id='set'>
-        <Button variant="contained" onClick={()=>{abc();}}>Hello World</Button>
-      </div>
     </div>
     
     
---
+++ my-portfolio/src/portfolio/Portfolio-under1000.js
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Skeleton from '@mui/material/Skeleton';
+
+const data = [
+    {
+        url: 'https://www.google.com/', //自分の作品のURLをここに
+        src: 'https://i.ytimg.com/vi/pLqipJNItIo/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBkklsyaw9FxDmMKapyBYCn9tbPNQ',//画像
+        title: 'Don Diablo @ Tomorrowland Main Stage 2019 | Official…',//タイトル
+        channel: 'Don Diablo',
+        views: '396k views',
+        createdAt: 'a week ago',
+    },
+    {
+        url: 'https://magazine.techacademy.jp/magazine/48764',
+        src: 'https://i.ytimg.com/vi/_Uu12zY01ts/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCpX6Jan2rxrCAZxJYDXppTP4MoQA',
+        title: 'Queen - Greatest Hits',
+        channel: 'Queen Official',
+        views: '40M views',
+        createdAt: '3 years ago',
+    },
+    {
+        url: 'https://mui.com/material-ui/react-skeleton/',
+        src: 'https://i.ytimg.com/vi/kkLk2XWMBf8/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLB4GZTFu1Ju2EPPPXnhMZtFVvYBaw',
+        title: 'Calvin Harris, Sam Smith - Promises (Official Video)',
+        channel: 'Calvin Harris',
+        views: '130M views',
+        createdAt: '10 months ago',
+    }
+    
+];
+
+function Media(props) {
+    const { loading = false } = props;
+
+    return (
+        <Grid container wrap="nowrap">
+        {(loading ? Array.from(new Array(0)) : data).map((item, index) => (
+            <Box key={index} sx={{ width: 210, marginRight: 0.5, my: 5 }}>
+            {item ? (
+                <a href={item.url}>
+                    <img
+                    style={{ width: 210, height: 118 }}
+                    alt={item.title}
+                    src={item.src}
+                    />
+                </a>
+            ) : (
+                <Skeleton variant="rectangular" width={210} height={118} />
+            )}
+
+            {item ? (
+                <Box sx={{ pr: 2 }}>
+                <Typography gutterBottom variant="body2">
+                    {item.title}
+                </Typography>
+                <Typography display="block" variant="caption" color="text.secondary">
+                    {item.channel}
+                </Typography>
+                <Typography variant="caption" color="text.secondary">
+                    {`${item.views} • ${item.createdAt}`}
+                </Typography>
+                </Box>
+            ) : (
+                <Box sx={{ pt: 0.5 }}>
+                <Skeleton />
+                <Skeleton width="60%" />
+                </Box>
+            )}
+            </Box>
+        ))}
+        </Grid>
+    );
+}
+
+Media.propTypes = {
+    loading: PropTypes.bool,
+};
+
+export default function YouTube() {
+    return (
+        <Box sx={{ overflow: 'hidden' }}>
+        <Media loading />
+        <Media />
+        </Box>
+    );
+}
