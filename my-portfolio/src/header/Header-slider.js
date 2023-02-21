3baac8522f27e6e176dd80e614e6c6277b052746
yasu hiromu <g020c1132@g.neec.ac.jp>
2023-02-21 18:11:36 +0900

    version.02

--- my-portfolio/src/header/Header-slider.js
+++ my-portfolio/src/header/Header-slider.js
@@ -2,36 +2,37 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Image00 from '../image/head01.jpg' 
-import Image01 from '../image/head.jpg' 
+import Image00 from '../image/my-img6.jpg' 
+import Image02 from '../image/my-img.jpg' 
+import Image03 from '../image/my-img2.jpg' 
+import Image04 from '../image/my-img3.jpg'
+import Image05 from '../image/my-img4.jpg'
 export default function SimpleSlider() {
     const settings = {
         autoplay: true,
         dots: true,
         infinite: true,
-        speed: 500,
+        interval: 2000,
+        speed: 1000,
         slidesToShow: 1,
         slidesToScroll: 1
     };
     return (
         <Slider {...settings}>
         <div>
-            <img src={Image00} alt="Logo" width="100%"/>
+            <img src={Image00} alt="自己分析" width="100%"/>
         </div>
         <div>
-            <img src={Image01} alt="Logo" width="50%"/>
+            <img src={Image02} alt="自己分析" width="100%"/>
         </div>
         <div>
-            <h3>3</h3>
+            <img src={Image03} alt="自己分析" width="100%"/>
         </div>
         <div>
-            <h3>4</h3>
+            <img src={Image04} alt="自己分析" width="100%"/>
         </div>
         <div>
-            <h3>5</h3>
-        </div>
-        <div>
-            <h3>6</h3>
+            <img src={Image05} alt="自己分析" width="100%"/>
         </div>
         </Slider>
     );
